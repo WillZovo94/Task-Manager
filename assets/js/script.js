8ef149401f2a828b55bf49992dede197d8317d20
William <bzfishmaster94@gmail.com>
2024-05-29 18:17:26 -0400

    Added a custom modal to to help gather information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+const taskTitleInput = document.querySelector('#task-title-input');
+const taskDateInput = document.querySelector('#task-date-input');
+const taskDescriptionInput = document.querySelector('#task-description-input');
+const addTaskBtn = document.querySelector(".btn-success");
+const cancelTaskBtn = document.querySelector("#cancel-btn");
+const addTaskInputBtn = document.querySelector("#add-task-btn")
+
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
@@ -35,4 +43,14 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
+
 });
+
+addTaskBtn.addEventListener('click', function() {
+    const modal = document.querySelector('#official-modal')
+    const overlay = document.querySelector('#overlay-div');
+    modal.setAttribute('style', 'visibility: visible');
+    overlay.setAttribute('style', 'visibility: visible');
+  })
+
+
